1fe17f6b8527f920f3c3996d443cedb8a932ac52
Luni-4 <luni-4@hotmail.it>
2020-12-10 22:11:22 +0100

    Do not count Rust unit expressions for lloc

--- src/metrics/loc.rs
+++ src/metrics/loc.rs
@@ -308,7 +308,6 @@ impl Loc for RustCode {
             | AssignmentExpression
             | CompoundAssignmentExpr
             | ReturnExpression
-            | UnitExpression
             | IfExpression
             | IfLetExpression
             | WhileExpression
@@ -589,6 +588,11 @@ mod tests {
             loc,
             [(lloc, 1, usize)]
         );
+    }
+
+    #[test]
+    fn rust_no_unit_expression_lloc() {
+        check_metrics!("let a = ();", "foo.rs", RustParser, loc, [(lloc, 1, usize)]);
     }
 
     #[test]
